0a19e906a55b761d55c7353104abff8f988cb668
Jean-Marc Reynaud <jean-marc.reynaud@yandex.com>
2020-09-18 10:19:04 +0100

    @SpringBootApplication same as @Configuration @EnableAutoConfiguration @ComponentScan

--- camel-openapi-reproducer/src/main/java/c10y/camel/ScratchTestApp.java
+++ camel-openapi-reproducer/src/main/java/c10y/camel/ScratchTestApp.java
@@ -4,13 +4,8 @@ import org.apache.camel.CamelContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan({"c10y.*"})
-@EntityScan("c10y.*")
-//@EnableJpaRepositories("c10y.*") for JPA
+@SpringBootApplication(scanBasePackages = {"c10y.*"})	// same as @Configuration @EnableAutoConfiguration @ComponentScan 
 public class ScratchTestApp {
 
 	@Autowired
